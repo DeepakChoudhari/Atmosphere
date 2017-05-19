1a03a7c1b412ebe479fb559574e2aefad7195d24
Deepak Choudhari <Deepak.Choudhari@outlook.com>
2017-05-19 17:34:24 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    'selector': 'angular-app',
+    'templateUrl': 'app/app.component.html'
+})
+export class AppComponent {
+    theDate = new Date();
+ }
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+// Main entry point for the application
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
