ffc3b288c20f139fdb7ccbde5f529d1aa0234052
Deepak Choudhari <Deepak.Choudhari@outlook.com>
2017-05-30 15:43:47 -0500

    Updated view to include weather data for 3 places by default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
+    'moduleId': module.id,
     'selector': 'angular-app',
-    'templateUrl': 'app/app.component.html'
+    'templateUrl': 'app.component.html',
+    'styles': ['a label {cursor: pointer;}'],
+    'providers': [/*  Services that this component consumes go here */]
 })
 export class AppComponent {
     weatherDataJson : string = '{"location":{"name":"Paris","region":"Ile-de-France","country":"France","lat":48.87,"lon":2.33,"tz_id":"Europe/Paris","localtime_epoch":1495078661,"localtime":"2017-05-18 5:37"},"current":{"last_updated_epoch":1495078207,"last_updated":"2017-05-18 05:30","temp_c":16.0,"temp_f":60.8,"is_day":0,"condition":{"text":"Moderate rain","icon":"//cdn.apixu.com/weather/64x64/night/302.png","code":1189},"wind_mph":5.6,"wind_kph":9.0,"wind_degree":230,"wind_dir":"SW","pressure_mb":1013.0,"pressure_in":30.4,"precip_mm":0.8,"precip_in":0.03,"humidity":88,"cloud":75,"feelslike_c":16.0,"feelslike_f":60.8,"vis_km":10.0,"vis_miles":6.0}}';
 
-    weatherData = new WeatherData(this.weatherDataJson);
+    weatherData = new WeatherDataModel(this.weatherDataJson);
+    cityName: string;
+
+    onSubmit()
+    {
+        console.log(this.cityName);
+    }
 }
 
-class WeatherData
+class WeatherDataModel
 {
     constructor(data: string) {
         try 
